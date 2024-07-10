0e6d14af5fab7bc18def5a81577baed033de464a
David Ward <David.Ward@standard.com>
2024-07-09 18:31:34 -0700

    Begin adding story parts

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,14 @@
 use std::io::{self, Write}; // Import Write to flush stdout
-use std::process::Command;
+
+// imports from other modules
+mod utils;
+use utils::handle_choice;
+use utils::clear_screen;
+use utils::quit_game;
+
+const GO_OR_STAY_LIST: [&str; 3] = ["GO", "STAY", "BACK"];
+const HALL_MATCHES_OR_LIGHTER: [&str; 2] = ["MATCHES", "LIGHTER"];
+const LEFT_RIGHT_OR_TEDDY_BEAR: [&str; 5] = ["LEFT", "RIGHT", "TEDDY BEAR", "BEAR", "TEDDY"];
 
 fn main() {
     clear_screen();
@@ -14,17 +23,31 @@ fn main() {
 
     // Call the stdin function from the io module to get user input
     // Read a line of input into the `name` variable
-    io::stdin() 
-        .read_line(&mut name) 
-        .expect("Failed to read line"); // Handle potential errors
+    io::stdin().read_line(&mut name).expect("Failed to read line"); // Handle potential errors
+    name = name.to_uppercase();
 
-    // Use trim to remove any newline characters
     println!("\nHello, {}. Welcome to The Nightmare!!", name.trim().to_uppercase()); 
     println!("Let's begin....\n");
 
-
+    // Starting location
+    bedroom(name);
 }
 
-fn clear_screen() {
-    Command::new("clear").status().unwrap();
-}   
+fn bedroom(name: String) {
+    println!("{}, you are in a bedroom", name);
+    println!("It's the middle of the night, and you startled awake by a noise coming from outside the room. What do you do?? GO check out the noise or STAY in bed?");
+    let choice = handle_choice(&GO_OR_STAY_LIST).to_string(); 
+    if choice == "STAY" {
+        quit_game();
+    } else if choice == "BACK" {
+        println!("You cannot go BACK from here.");
+        bedroom(name);
+    } else {
+        hall(name);
+    }
+}
+
+fn hall(name: String) {
+    println!("{}, you are in a hall", name);
+    println!("You see a door on the LEFT, a door on the RIGHT, and a TEDDY BEAR on the floor. Which do you choose? LEFT, RIGHT, or TEDDY BEAR?");
+}
---
+++ src/utils.rs
@@ -0,0 +1,34 @@
+use std::io::{self, Write}; // Import Write to flush stdout
+use std::process::Command;
+use std::process;
+
+pub fn clear_screen() {
+  Command::new("clear").status().unwrap();
+}
+
+pub fn handle_choice(choice_list: &[&str]) -> String {
+  let mut choice = String::new();
+
+  loop {
+      print!("> ");
+      io::stdout().flush().unwrap();
+      choice.clear();
+      io::stdin().read_line(&mut choice).expect("Failed to read line");
+      let choice = choice.trim().to_uppercase();
+      if choice == "QUIT" {
+          quit_game();
+          break; 
+      } else if !choice_list.contains(&choice.as_str()) {
+          println!("\nThat's not one of the choices. Your choices are: {:?}", choice_list);
+      } else {
+          break;
+      }
+  }
+  return choice.trim().to_string();
+}
+
+pub fn quit_game() {
+  println!("Quitting game...");
+  // Exit the program with a success code
+  process::exit(0); 
+}

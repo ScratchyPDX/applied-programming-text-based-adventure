8f6d5590a96c8187a77b11fc7a7fe9d8ad82ef8a
David Ward <David.Ward@standard.com>
2024-07-11 15:33:22 -0700

    Add ability to colorize command text

--- src/bedroom.rs
+++ src/bedroom.rs
@@ -3,6 +3,7 @@ use crate::utils::handle_choice;
 use crate::utils::print_wrapped_text;
 mod hall;
 use hall::Hall;
+use colored::*;
 
 pub struct Bedroom {
   player_name: String
@@ -17,7 +18,13 @@ impl Bedroom {
   pub fn enter(&self) {
     const GO_OR_STAY_LIST: [&str; 3] = ["GO", "STAY", "BACK"];
     println!("{}, you are in a bedroom", self.player_name);
-    print_wrapped_text("It's the middle of the night, and you startled awake by a noise coming from outside the room. What do you do?? GO check out the noise or STAY in bed?");
+    print_wrapped_text(
+      &format!("{} {} {} {} {}", 
+        "It's the middle of the night, and you startled awake by a noise coming from outside the room. What do you do??", 
+        GO_OR_STAY_LIST[0].green(), 
+        "check out the noise or", 
+        GO_OR_STAY_LIST[1].green(), 
+        "in bed?"));
     let choice = handle_choice(&GO_OR_STAY_LIST).to_string(); 
     if choice == "STAY" {
         quit_game();
